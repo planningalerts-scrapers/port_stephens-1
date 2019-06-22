2acac12ad41c0da510cf7af52ba508cdac6e71ae
Matthew Landauer <mlandauer@gmail.com>
2019-06-22 21:24:41 +1000

    Upgrade to latest gem

--- scraper.rb
+++ scraper.rb
@@ -1,9 +1,3 @@
 require "masterview_scraper"
 
-MasterviewScraper.scrape_and_save_period(
-  url: "http://datracker.portstephens.nsw.gov.au",
-  period: :thismonth,
-  use_api: true,
-  long_council_reference: true,
-  types: [16, 9, 25]
-)
+MasterviewScraper.scrape_and_save(:port_stephens)
